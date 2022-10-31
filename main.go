105ec18896f240be7c920975f50555d0b2d48852
koushengrui <koushengrui@hongtu.tech>
2022-10-31 11:16:53 +0800

    研究signal.Notify

--- main.go
+++ main.go
@@ -2,29 +2,23 @@ package main
 
 import (
 	"fmt"
-	"github.com/corazawaf/coraza/v2"
-	"github.com/corazawaf/coraza/v2/seclang"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
-	// First we initialize our waf and our seclang parser
-	waf := coraza.NewWaf()
-	parser, _ := seclang.NewParser(waf)
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool)
 
-	// Now we parse our rules
-	if err := parser.FromString(`SecRule REMOTE_ADDR "@rx .*" "id:1,phase:1,deny,status:403"`); err != nil {
-		fmt.Println(err)
-	}
+	signal.Notify(sigs, syscall.SIGINT)
 
-	// Then we create a transaction and assign some variables
-	tx := waf.NewTransaction()
-	defer func() {
-		tx.ProcessLogging()
-		tx.Clean()
+	go func() {
+		sig := <-sigs
+		fmt.Println("shutting down, caused by", sig)
+		close(done)
 	}()
-	tx.ProcessConnection("127.0.0.1", 8080, "127.0.0.1", 12345)
-	// Finally we process the request headers phase, which may return an interruption
-	if it := tx.ProcessRequestHeaders(); it != nil {
-		fmt.Printf("Transaction was interrupted with status %d\n", it.Status)
-	}
+
+	<-done
+	fmt.Println("Graceful shutdown.")
 }
